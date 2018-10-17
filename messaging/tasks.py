518324ae23b19c96805e1ca0121a0aa8e74e02fd
Svante Nilsson <svante.n@live.se>
2018-10-17 14:54:26 +0200

    fix

--- messaging/tasks.py
+++ messaging/tasks.py
@@ -11,8 +11,8 @@ def gen_prime(x):
     multiples = []
     results = []
     for i in xrange(2, x+1):
-	if i not in multiples:
-	    results.append(i)
-	    for j in xrange(i*i, x+1, i):
-		multiples.append(j)
+        if i not in multiples:
+            results.append(i)
+            for j in xrange(i*i, x+1, i):
+                multiples.append(j)
     return results
