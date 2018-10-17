eba58df1c14e384a20fac7da736c0eedc238a004
Svante Nilsson <svante.n@live.se>
2018-10-17 14:36:27 +0200

    revert tasks

--- messaging/tasks.py
+++ messaging/tasks.py
@@ -1,35 +1,6 @@
 from celery import Celery
 
-def make_celery(app):
-    celery = Celery(
-        app.import_name,
-        backend=app.config['CELERY_RESULT_BACKEND'],
-        broker=app.config['CELERY_BROKER_URL']
-    )
-    celery.conf.update(app.config)
-
-    class ContextTask(celery.Task):
-        def __call__(self, *args, **kwargs):
-            with app.app_context():
-                return self.run(*args, **kwargs)
-
-    celery.Task = ContextTask
-    return celery
-
 app = Celery('tasks', backend='amqp', broker='amqp://')
-
-from flask import Flask
-
-flask_app = Flask(__name__)
-flask_app.config.update(
-    CELERY_BROKER_URL='amqp://',
-    CELERY_RESULT_BACKEND='amqp://'
-)
-celery = make_celery(flask_app)
-
-@celery.task()
-def add_together(a, b):
-    return a + b
 
 @app.task(ignore_result=True)
 def print_hello():
