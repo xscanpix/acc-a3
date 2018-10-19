25aed63ffe510c2dbdcaee028adef4882c146a5f
Svante Nilsson <svante.n@live.se>
2018-10-19 14:15:31 +0200

    m

--- messaging/tasks.py
+++ messaging/tasks.py
@@ -25,8 +25,8 @@ def gen_prime(x):
     return str(results) + '\n'
 
 @celery.task
-def return_text():
-	with open("/home/ubuntu/data/0c7526e6-ce8c-4e59-884c-5a15bbca5eb3", 'r') as infile:
+def return_text(data_path):
+	with open(data_path, 'r') as infile:
 		rows = infile.readlines()
 
 	filtered = list(filter(lambda x: x != '\n', rows))
@@ -36,7 +36,29 @@ def return_text():
 
 @app.route('/text', methods=['GET'])
 def text():
-	return return_text()
+
+	num_workers = 5
+
+	data_paths = ["data/05cb5036-2170-401b-947d-68f9191b21c6",
+				  "data/094b1612-1832-429e-98c1-ae06e56d88d6", 
+				  "data/0c7526e6-ce8c-4e59-884c-5a15bbca5eb3",
+				  "data/0d7c752e-d2a6-474b-aef4-afe5dc506e33",
+				  "data/0ecdf8e0-bc1a-4fb3-a015-9b8dc563a92f"]
+
+	result = []
+
+	result = return_text(data_paths[0]).delay(1,2)
+
+	print Task finished?, result.ready()
+	print Task result, result.result
+
+	sleep(10)
+
+	print Task finished?, result.ready()
+	print Task result, result.result
+
+
+	return result.result
 
 @app.route('/prime', methods=['GET'])
 def prime():
