3dcdc2ef84650cb2f05b1c8c361dd894b4f7a4a0
Svante Nilsson <svante.n@live.se>
2018-10-20 15:08:16 +0200

    docker

--- proj/tasks.py
+++ proj/tasks.py
@@ -4,25 +4,18 @@ from celery import shared_task
 def hello():
     return 'Hello there!'
 
-@shared_task
-def gen_prime(x):
-    multiples = []
-    results = []
-    for i in xrange(2, x+1):
-        if i not in multiples:
-            results.append(i)
-            for j in xrange(i*i, x+1, i):
-                multiples.append(j)
-    return str(results) + '\n'
+@shared_task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
 
 @shared_task
 def return_text(data_path):
-	with open(data_path, 'r') as infile:
-		rows = infile.readlines()
+    with open(data_path, 'r') as infile:
+        rows = infile.readlines()
 
-	filtered = list(filter(lambda x: x != '\n', rows))
-	text = list(map(lambda x: json.loads(x)['text'].encode('utf-8'), filtered))
+    filtered = list(filter(lambda x: x != '\n', rows))
+    text = list(map(lambda x: json.loads(x)['text'].encode('utf-8'), filtered))
 
-	return str(text)
+    return str(text)
 
 
