85ba6a4890e34baf7d78ce026d7eef17dcfcef1a
Svante Nilsson <svante.n@live.se>
2018-10-20 15:04:13 +0200

    docker

--- proj/app.py
+++ proj/app.py
@@ -16,16 +16,6 @@ if settings_override:
 celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'],
                 include=CELERY_TASK_LIST)
 celery.conf.update(app.config)
-TaskBase = celery.Task
-
-class ContextTask(TaskBase):
-    abstract = True
-
-    def __call__(self, *args, **kwargs):
-        with app.app_context():
-            return TaskBase.__call__(self, *args, **kwargs)
-
-celery.Task = ContextTask
 
 @app.route('/prime', methods=['GET'])
 def prime():
