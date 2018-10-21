32aec6ca81d35b713a6842a4dd0a650917631082
Svante Nilsson <svante.n@live.se>
2018-10-21 17:34:22 +0200

    freq

--- messaging/tasks.py
+++ messaging/tasks.py
@@ -16,61 +16,56 @@ def debug_task(self):
 
 @celery.task
 def return_text(data_path):
-	with open(data_path, 'r') as infile:
-		rows = infile.readlines()
+  with open(data_path, 'r') as infile:
+    rows = infile.readlines()
 
-	filtered = list(filter(lambda x: x != '\n', rows))
-	text = list(map(lambda x: json.loads(x)['text'].encode('utf-8'), filtered))
-	text_no_rt = list(filter(lambda x: x[:2] != 'RT', text))
+  filtered = list(filter(lambda x: x != '\n', rows))
+  text = list(map(lambda x: json.loads(x)['text'].encode('utf-8'), filtered))
+  text_no_rt = list(filter(lambda x: x[:2] != 'RT', text))
 
   pronouns = {'han': 0,'hon': 0,'hen': 0,'det': 0,'denna': 0,'denne': 0,'den': 0}
 
   for row in text_no_rt:
     pronouns = count_words(pronouns, row)        
-	
-	pronouns_json = json.dumps(pronouns)
+
+  pronouns_json = json.dumps(pronouns)
         
-	return pronouns_json
+  return pronouns_json
 
 def count_words(pronouns, text):
-	words = str.split(text)
-	for word in words:
-		if word in pronouns:
-			pronouns[word] += 1
-	return pronouns
+  words = str.split(text)
+  for word in words:
+    if word in pronouns:
+      pronouns[word] += 1
+  return pronouns
 
 @celery.task
 def print_hello():
 
-	return 'Hello there!\n'
+  return 'Hello there!\n'
 
 @app.route('/text', methods=['GET'])
 def text():
 
-	num_workers = 5
+  num_workers = 5
 
-	data_paths = ["/home/ubuntu/data/05cb5036-2170-401b-947d-68f9191b21c6",
-				  "/home/ubuntu/data/094b1612-1832-429e-98c1-ae06e56d88d6", 
-				  "/home/ubuntu/data/0c7526e6-ce8c-4e59-884c-5a15bbca5eb3",
-				  "/home/ubuntu/data/0d7c752e-d2a6-474b-aef4-afe5dc506e33",
-				  "/home/ubuntu/data/0ecdf8e0-bc1a-4fb3-a015-9b8dc563a92f"]
+  data_paths = ["/home/ubuntu/data/05cb5036-2170-401b-947d-68f9191b21c6",
+                "/home/ubuntu/data/094b1612-1832-429e-98c1-ae06e56d88d6",
+                "/home/ubuntu/data/0c7526e6-ce8c-4e59-884c-5a15bbca5eb3",
+                "/home/ubuntu/data/0d7c752e-d2a6-474b-aef4-afe5dc506e33",
+                "/home/ubuntu/data/0ecdf8e0-bc1a-4fb3-a015-9b8dc563a92f"]
 
-	result = return_text.delay(data_paths[0])
+  result = return_text.delay(data_paths[0])
+  print "Task finished? ", str(result.ready())
+  time.sleep(5)
+  print "Task finished? " + str(result.ready())
 
-	print "Task finished? ", str(result.ready())
-
-	time.sleep(5)
-
-	print "Task finished? " + str(result.ready())
-
-
-
-	return result.result
+  return result.result
 
 
 @app.route('/hello', methods=['GET'])
 def hello():
 	return print_hello()
-
+  
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', debug=True, port=5000)
+  app.run(host='0.0.0.0', debug=True, port=5000)
