4992fa2373d48d6c0452895628cdefdae9046914
Svante Nilsson <svante.n@live.se>
2018-10-21 17:31:59 +0200

    freq

--- messaging/tasks.py
+++ messaging/tasks.py
@@ -23,15 +23,7 @@ def return_text(data_path):
 	text = list(map(lambda x: json.loads(x)['text'].encode('utf-8'), filtered))
 	text_no_rt = list(filter(lambda x: x[:2] != 'RT', text))
 
-  pronouns = {
-    'han': 0,
-		'hon': 0,
-    'hen': 0,
-    'det': 0,
-    'denna': 0,
-    'denne': 0,
-    'den': 0 }
-
+  pronouns = {'han': 0,'hon': 0,'hen': 0,'det': 0,'denna': 0,'denne': 0,'den': 0}
 
   for row in text_no_rt:
     pronouns = count_words(pronouns, row)        
