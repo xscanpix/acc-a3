f1de373107c1765303497b2f54fef2fb07cd46c8
Svante Nilsson <svante.n@live.se>
2018-10-21 14:24:05 +0200

    project structure

--- build-a-saas-app-with-flask-master/snakeeyes/extensions.py
+++
@@ -1,7 +0,0 @@
-from flask_debugtoolbar import DebugToolbarExtension
-from flask_mail import Mail
-from flask_wtf import CSRFProtect
-
-debug_toolbar = DebugToolbarExtension()
-mail = Mail()
-csrf = CSRFProtect()
