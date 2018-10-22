781e3d0bf7ec5b665c2d2bb002c515c1fb8e3ca5
Svante Nilsson <svante.n@live.se>
2018-10-22 14:32:55 +0200

    m

--- messaging/tasks.py
+++ messaging/tasks.py
@@ -77,45 +77,12 @@ def task(self):
   for path in data_paths:
     result.append(return_text(path))
 
-  return {'current': 100, 'total': 100, 'status': 'Task completed!',
-            'result': 42}
+  return result
 
 @app.route('/longtask', methods=['GET'])
 def longtask():
-  task = count_all_words.apply_async()
-  return jsonify({}), 202, {'Location': url_for('taskstatus',
-                                                  task_id=task.id)}
-
-@app.route('/status/<task_id>')
-def taskstatus(task_id):
-  task = long_task.AsyncResult(task_id)
-  if task.state == 'PENDING':
-    # job did not start yet
-    response = {
-        'state': task.state,
-        'current': 0,
-        'total': 1,
-        'status': 'Pending...'
-    }
-  elif task.state != 'FAILURE':
-    response = {
-        'state': task.state,
-        'current': task.info.get('current', 0),
-        'total': task.info.get('total', 1),
-        'status': task.info.get('status', '')
-    }
-    if 'result' in task.info:
-        response['result'] = task.info['result']
-  else:
-    # something went wrong in the background job
-    response = {
-        'state': task.state,
-        'current': 1,
-        'total': 1,
-        'status': str(task.info),  # this is the exception raised
-    }
-  return jsonify(response)
-
+  res = task.apply_async()
+  return res
   
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True, port=5000)
