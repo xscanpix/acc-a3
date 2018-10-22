68d5e07cb6cf0dd4254ad451a8afbe0b76c81124
Svante Nilsson <svante.n@live.se>
2018-10-22 12:56:24 +0200

    m

--- messaging/tasks.py
+++ messaging/tasks.py
@@ -31,9 +31,7 @@ def count_all_words():
                 "/home/ubuntu/data/0d7c752e-d2a6-474b-aef4-afe5dc506e33",
                 "/home/ubuntu/data/0ecdf8e0-bc1a-4fb3-a015-9b8dc563a92f"]
 
-  result = group(return_text.s(t) for t in data_paths)()
-
-  return result
+ 	return group(return_text.s(t) for t in data_paths).delay()
 
 @shared_task(trail=True)
 def return_text(data_path):
@@ -63,27 +61,12 @@ def count_words(pronouns, text):
 
 @app.route('/count', methods=['GET'])
 def count():
-	result = count_all_words.delay()
+	result = count_all_words()
+
+	print result
 
 	return str(result)
 
-@app.route('/text', methods=['GET'])
-def text():
-
-  num_workers = 5
-
-  data_paths = ["/home/ubuntu/data/05cb5036-2170-401b-947d-68f9191b21c6",
-                "/home/ubuntu/data/094b1612-1832-429e-98c1-ae06e56d88d6",
-                "/home/ubuntu/data/0c7526e6-ce8c-4e59-884c-5a15bbca5eb3",
-                "/home/ubuntu/data/0d7c752e-d2a6-474b-aef4-afe5dc506e33",
-                "/home/ubuntu/data/0ecdf8e0-bc1a-4fb3-a015-9b8dc563a92f"]
-
-  result = return_text.delay(data_paths[0])
-
-  while(result.ready() != True):
-  	time.sleep(5)
-
-  return result
   
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True, port=5000)
