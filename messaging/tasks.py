44fffabe055aec040e6981ffbe3a1359f1e5b6d4
Svante Nilsson <svante.n@live.se>
2018-10-22 11:42:39 +0200

    freq

--- messaging/tasks.py
+++ messaging/tasks.py
@@ -1,4 +1,4 @@
-from celery import Celery
+from celery import Celery, group
 from flask import Flask
 import json
 import time
@@ -14,7 +14,20 @@ celery = Celery(app.name, backend=app.config['CELERY_RESULT_BACKEND'], broker=ap
 def debug_task(self):
   print('Request: {0!r}'.format(self.request))
 
-@celery.task
+@celery.task(trail=True)
+def count_all_words():
+	data_paths = ["/home/ubuntu/data/05cb5036-2170-401b-947d-68f9191b21c6",
+                "/home/ubuntu/data/094b1612-1832-429e-98c1-ae06e56d88d6",
+                "/home/ubuntu/data/0c7526e6-ce8c-4e59-884c-5a15bbca5eb3",
+                "/home/ubuntu/data/0d7c752e-d2a6-474b-aef4-afe5dc506e33",
+                "/home/ubuntu/data/0ecdf8e0-bc1a-4fb3-a015-9b8dc563a92f"]
+
+
+  result = group(return_text.s(t) for t in data_paths)()
+
+  return result
+
+@celery.task(trail=True)
 def return_text(data_path):
   with open(data_path, 'r') as infile:
     rows = infile.readlines()
@@ -26,7 +39,7 @@ def return_text(data_path):
   pronouns = {'han': 0,'hon': 0,'hen': 0,'det': 0,'denna': 0,'denne': 0,'den': 0}
 
   for row in text_no_rt:
-    pronouns = count_words(pronouns, row)        
+    pronouns = count_words(pronouns, row)       
 
   result = {'file': data_path, 'pronoun_counts': pronouns}
   pronouns_json = json.dumps(result)
